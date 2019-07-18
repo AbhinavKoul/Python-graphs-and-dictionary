d6b2a1ebd94df7a7721dda7f96e60241738a1245
Abhinav <abhinav2scientist@gmail.com>
2019-07-19 01:17:16 +0530

    string comparision observation and result

---
+++ 4 Logic,Control Flow and filtering/1.py
@@ -0,0 +1,5 @@
+print("chris"<"charles")     #FALSE
+print("caris"<"charles")     #TRUE 
+
+#Cause point of check starts from 1st character and if the 1st character is same it moves on to the next character and check
+# At the 1st point of diffirence the comparision is made on the basis of alphabetical order.
