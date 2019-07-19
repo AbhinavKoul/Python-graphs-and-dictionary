c0124fd91d067c77b0cc7fa595a08d861d6d3779
Abhinav <abhinav2scientist@gmail.com>
2019-07-19 18:25:46 +0530

    user friendly output

--- 4 Logic,Control Flow and filtering/6 enumerate fn in for loop(2 values).py
+++ 4 Logic,Control Flow and filtering/6 enumerate fn in for loop(2 values).py
@@ -4,3 +4,9 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Change for loop to use enumerate() and update print()
 for index,a in enumerate(areas) :
     print("room"+str(index)+":"+str(a))
+
+#since in previous it prints room 0 which makes no sence to other people
+# Code the for loop
+for index, area in enumerate(areas) :
+    index = index + 1
+    print("room " + str(index) + ": " + str(area))
